55708e789559481c9ac3dc2e7968f84015b9962c
Alexander Hauser <alexander.hauser@hs-furtwangen.de>
2021-01-21 10:33:11 +0100

    Update/Frontend Changes for Blinds

--- src/frontend/helper-functions.js
+++ src/frontend/helper-functions.js
@@ -8,3 +8,12 @@ function uhr() {
   var minuten = jetzt.getMinutes();
   document.getElementById("uhr").innerHTML = stunden + ":" + minuten + " Uhr";
 }
+
+setInterval(() => {
+  document.getElementById('valBathroom').value = document.getElementById('sldBathroom').value;
+  document.getElementById('valKitchen').value = document.getElementById('sldKitchen').value;
+  document.getElementById('valBedroom').value = document.getElementById('sldBedroom').value;
+  document.getElementById('valLivingroom').value = document.getElementById('sldLivingroom').value;
+}, 100); 
+
+
