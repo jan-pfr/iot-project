0c4c85fa6b47b2d06ce28e427476aaf5e687b01c
henrikKar <76650440+henrikKar@users.noreply.github.com>
2021-01-24 12:46:26 +0100

    Update heating.js

--- src/frontend/heating.js
+++ src/frontend/heating.js
@@ -182,6 +182,7 @@ function updateBlindsValues(room, properties) {
   $(`.blinds .${room} .title`).html(room);
   $(`.blinds .${room} .mode`).attr("data-value", properties.mode);
   $(`.blinds .${room} .mode`).html(properties.mode ? "Automatic" : "Manual");
-  $(`.blinds .${room} .target`).html(properties.target)
+  $(`.blinds .${room} .sld`).html(properties.target)
+  $(`.blinds .${room} .val`).html(properties.target)
 
 }
