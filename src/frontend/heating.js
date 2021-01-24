ad3322ca792a411167cea44a3de996193eb656ae
Alexander Hauser <alexander.hauser@hs-furtwangen.de>
2021-01-24 20:03:48 +0100

    Update Frontend/Sorry Jan

--- src/frontend/heating.js
+++ src/frontend/heating.js
@@ -48,13 +48,13 @@ $(() => {
     }
     if(hasLocationChanged || hasEventChanged || !hasAlertBeenShown){
       currentEvent = message.event;
-      $(`.alert-overlay .alert-content .event`).html(message.event);
-      $(`.alert-overlay .alert-content .sender`).html(message.sender_name);
-      $(`.alert-overlay .alert-content .period`).html(convertUnixTimestamp(message.start) +" - "+convertUnixTimestamp(message.end));
-      $(`.alert-overlay .alert-content .description`).html(message.description);
-      $(".alert-overlay, .alert-content").addClass("active");
+      $(`.alert-content .event`).html(message.event);
+      $(`.alert-content .sender`).html(message.sender_name);
+      $(`.alert-content .period`).html(convertUnixTimestamp(message.start) +" - "+convertUnixTimestamp(message.end));
+      $(`.alert-content .description`).html(message.description);
+      $(".alert-content").addClass("active");
       $(".close").on("click", function() {
-        $(".alert-overlay, .alert-content").removeClass("active");
+        $(".alert-content").removeClass("active");
       });
       hasAlertBeenShown = true;
       //currently this boolean is set manually. later it will be changed automatically.
